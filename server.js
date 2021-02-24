984fecf8b8972ca7973a5fea8af9e0d94c577739
h4moka <64992959+h4moka@users.noreply.github.com>
2021-02-25 02:10:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -60,4 +60,4 @@ client.on("message", message => {
 
 
 //DONT DO ANYTHING WITH THIS TOKEN lol
-client.login(NzQ3MTUyMTM4NTQyNTE0MTg5.X0Ktpw.ZWOtrJ8s9NQnhStNYVC2OsboJEI)
+client.login(NzQ3MTUyMTM4NTQyNTE0MTg5.X0Ktpw.Cm6uOSuVDuBOW2ha3KqFmbpULOg)
